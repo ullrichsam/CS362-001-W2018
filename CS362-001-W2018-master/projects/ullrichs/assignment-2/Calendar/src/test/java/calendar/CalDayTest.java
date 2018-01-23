9065cc8b6341b05ad612b654382ead22c78954c5
ullrichsam <ullrichs@oregonstate.edu>
2018-01-23 00:18:25 -0800

    CalDayTest has three finished tests

--- CS362-001-W2018-master/projects/ullrichs/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
+++ CS362-001-W2018-master/projects/ullrichs/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -5,6 +5,7 @@ package calendar;
  */
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 
 import org.junit.Test;
@@ -13,13 +14,31 @@ import static org.junit.Assert.*;
 
 public class CalDayTest {
 
-	 @Test
-	  public void test01()  throws Throwable  {
+	 private LinkedList<Appt> appts;
 
-	 }
-	 @Test
-	  public void test02()  throws Throwable  {
+	@Test
+	  public void CalDayMethodTest(GregorianCalendar cal)  throws Throwable  {
+		 CalDay calDay = new CalDay();
 		 
+		assertEquals(cal.get(cal.DAY_OF_MONTH), calDay.getDay());
+		assertEquals(cal.get(cal.MONTH), calDay.getMonth());
+		assertEquals(cal.get(cal.YEAR), calDay.getYear());
+		assertTrue(calDay.isValid());
 	 }
+	 
+	 @Test
+	  public void addApptTest(Appt appt)  throws Throwable  {
+		 CalDay AddAppt = new CalDay();
+		 assertTrue(AddAppt.getAppts().add(appt));
+	 }
+	 
+	  @Test
+	  public void setApptsTest(LinkedList<Appt> appts)  throws Throwable  {
+		  CalDay SetAppt = new CalDay();
+		  assertTrue(SetAppt.isValid());
+		  if(appts!=null)
+				this.appts = appts;
+		  assertEquals(appts,SetAppt.appts);	  
+	  }
 //add more unit tests as you needed	
 }
--- CS362-001-W2018-master/projects/ullrichs/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
+++ CS362-001-W2018-master/projects/ullrichs/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.*;
 
 public class TimeTableTest {
 
-	
+	/*
 	 @Rule 
 	 public ExpectedException exception = ExpectedException.none();
 	 
@@ -33,23 +33,48 @@ public class TimeTableTest {
 	 }
 	 
 	 @Test
-	  public void test01()  throws Throwable  
+	  public void testGetApptRange()  throws Throwable  
 	 {
-		// exception.expect(DateOutOfRangeException.class);
-		// exception.expectMessage("");
-		// TimeTable.getApptRange(null, GregorianCalendar, null);
-		 
-		 
-		 
+		 TimeTable current = new TimeTable();
+		 System.out.println(current);
+		 exception.expect(DateOutOfRangeException.class);
+		 exception.expectMessage("Second date specified is not  before the first date specified.");
+		 //TimeTable.getApptRange(null, null, null);
+		 		 
 	 }
 
 	 @Test
-	  public void test02()  throws Throwable  {
-		 TimeTable current = new TimeTable();
-		    System.out.println(current);
-		    assertEquals("today:", current, new TimeTable());
-		 
+	  public void testGetApptRange2()  throws Throwable  {
+		 		LinkedList<CalDay> calDays = new LinkedList<CalDay>();
+		 		int day = 3;
+				int month = 05;
+				int year = 1981;
+				calendar.CalDay.setDay(day);
+			
+				
+		        final GregorianCalendar firstDay = new GregorianCalendar(2004, 6, 9, 13, 45);
+		        final GregorianCalendar lastDay = new GregorianCalendar(2004, 6, 9, 13, 45);
+		        assertTrue(TimeTable.getApptRange(LinkedList<CalDay>, firstDay, lastDay));
+		        lastDay.add(Calendar.DAY_OF_YEAR, 1);
+		        assertFalse(TimeTable.getApptRange(firstDay, lastDay));
+		        firstDay.add(Calendar.DAY_OF_YEAR, 1);
+		        assertTrue(TimeTable.getApptRange(firstDay, lastDay));
+		        lastDay.add(Calendar.YEAR, 1);
+		        assertFalse(TimeTable.getApptRange(firstDay, lastDay));
     }
+	  
+	  @Test
+	  public void testGetApptOccurences()  throws Throwable  {
+		  
+		  
+	  }
+	  
+	  @Test(expected = IllegalArgumentException.class)
+	    public void testIsSameDay_DateNullNull() throws Exception {
+	        TimeTable.result((Date) null, (Date) null);
+	    }
+
+	  */
     
 //add more unit tests as you needed
 }
